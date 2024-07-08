fd7523197c98eec3c2f45b801987a47bc74abb68
Raheemspb <gabibovda@gmail.com>
2024-07-08 15:53:25 +0300

    Create and setup tableView, add constraints, add dataSource and delegate.

---
+++ ItunseDisk/ItunseDisk/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  ItunseDisk
+//
+//  Created by Рахим Габибли on 08.07.2024.
+//
+
+import Foundation
--- ItunseDisk/ItunseDisk/ViewController.swift
+++ ItunseDisk/ItunseDisk/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
+        print("LL")
     }
 
+    private func setupTableView() {
+        tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 90
+
+        tableView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+        }
+
+    }
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        cell.textLabel?.text = "This \(indexPath)"
+        return cell
+    }
+    
+
+}
+
+extension ViewController: UITableViewDelegate {
 
 }
 
